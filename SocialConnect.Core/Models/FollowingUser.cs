d1d39ff793bb8d3242e6ed68dbf8ee9b321e9619
mohamed121helmy <mohamed121helmy@gmail.com>
2024-11-22 08:12:31 +0200

    v1

---
+++ SocialConnect.Core/Models/FollowingUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialConnect.Core.Models
+{
+    public class FollowingUser : BaseEntity<Guid>
+    {
+        [ForeignKey("user")]
+        public Guid MyID { get; set; }
+        public virtual User user { get; set; }
+        [ForeignKey("users")]
+        public Guid FollowinguseId_fk { get; set; }
+        public virtual User users { get; set; }
+    }
+}
